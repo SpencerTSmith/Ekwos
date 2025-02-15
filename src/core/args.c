f0bbc0ebfa1da62ca75dc87f082bcd142a501e3f
SpencerTSmith <spencertsmith2003@gmail.com>
2025-02-15 17:55:34 -0600

    make arg parsing a bit more robust

--- src/core/args.c
+++ src/core/args.c
@@ -6,10 +6,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Is this over engineering?
+static const char *arg_strings[] = {"--window-width", "--window-height"};
+static const char *arg_short_strings[] = {"-w", "-h"};
+
 Argument arg_from_string(char *arg) {
-  if (strcmp(arg, "--window-width") == 0 || strcmp(arg, "-w") == 0)
+  if (strcmp(arg, arg_strings[ARG_WINDOW_WIDTH]) == 0 ||
+      strcmp(arg, arg_short_strings[ARG_WINDOW_WIDTH]) == 0)
     return ARG_WINDOW_WIDTH;
-  if (strcmp(arg, "--window-height") == 0 || strcmp(arg, "-h") == 0)
+  if (strcmp(arg, arg_strings[ARG_WINDOW_HEIGHT]) == 0 ||
+      strcmp(arg, arg_short_strings[ARG_WINDOW_HEIGHT]) == 0)
     return ARG_WINDOW_HEIGHT;
 
   return ARG_INVALID;
@@ -22,33 +28,40 @@ Config arg_parse(u32 argc, char **argv) {
       .window_height = WINDOW_DEFAULT_HEIGHT,
   };
 
-  if (argc == 1) {
+  if (argc == 1)
     return config;
-  }
 
   for (u32 i = 1; i < argc; i++) {
     Argument arg = arg_from_string(argv[i]);
     switch (arg) {
     case ARG_WINDOW_WIDTH:
       if (i + 1 >= argc) {
-        LOG_ERROR("Please include a second argument for --window-width or -w");
+        LOG_ERROR("Please include a value for %s or %s", arg_strings[ARG_WINDOW_WIDTH],
+                  arg_short_strings[ARG_WINDOW_WIDTH]);
         continue;
       }
 
       // HACK(ss): Eventually think about replacing atoi, not always safe
       config.window_width = atoi(argv[i + 1]);
-      LOG_INFO("CLI: window width set to %lu", config.window_width);
+      LOG_INFO("Window width set to %lu", config.window_width);
+      i++; // We can skip the next argument
+
       break;
     case ARG_WINDOW_HEIGHT:
       if (i + 1 >= argc) {
-        LOG_ERROR("Please include a second argument for --window-height or -h");
+        LOG_ERROR("Please include a value for %s or %s", arg_strings[ARG_WINDOW_HEIGHT],
+                  arg_short_strings[ARG_WINDOW_HEIGHT]);
         continue;
       }
 
       // HACK(ss): Eventually think about replacing atoi, not always safe
       config.window_height = atoi(argv[i + 1]);
-      LOG_INFO("CLI: window height set to %lu", config.window_height);
+      LOG_INFO("Window height set to %lu", config.window_height);
+      i++; // We can skip the next argument
+
       break;
+    case ARG_INVALID:
+      LOG_ERROR("Unkown argument: \"%s\"", argv[i]);
     }
   }
 
