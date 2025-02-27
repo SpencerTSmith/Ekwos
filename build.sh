55ee2db4557ef71b595a91e6e095ad7de77785c0
SpencerTSmith <spencertsmith2003@gmail.com>
2025-02-26 22:29:31 -0600

    add jank incremental build to the build script

--- build.sh
+++ build.sh
@@ -5,31 +5,72 @@ PROJECT_NAME="ekwos"
 
 SRC_DIR="src"
 LIBS_DIR="libs"
-SHADER_DIR="src/shaders"
+SHADER_DIR="${SRC_DIR}/shaders"
 
 BIN_DIR="bin"
 OUTPUT_SHADER_DIR="${BIN_DIR}/shaders"
+OBJ_DIR="${BIN_DIR}/o"
+
+C_SOURCES=$(find "${SRC_DIR}" -name "*.c")
+LIB_SOURCES=$(find "${LIBS_DIR}" -name "*.c")
+SHADER_SRCS=$(find "${SHADER_DIR}" -name "*.vert" -o -name "*.frag")
+
+CFLAGS=" -g -Wall -Wextra -Wshadow -Wpedantic -DDEBUG=1 -DOS_LINUX=1 -std=gnu17"
+LDFLAGS="-lglfw -lvulkan -lm"
+
+OBJ_FILES=()
 
 mkdir -p "${BIN_DIR}"
 mkdir -p "${OUTPUT_SHADER_DIR}"
+mkdir -p "${OBJ_DIR}"
 
-SHADER_SRCS=$(find "${SHADER_DIR}" -name "*.vert" -o -name "*.frag")
+needs_rebuild() {
+	local source_file=$1
+	local object_file=$2
+
+	# Object doesn't exist yet
+	if [[ ! -f "${object_file}" ]]; then
+		return 1
+	fi
+
+	# Object is newer
+	if [[ ! "${source_file}" -nt "${object_file}" ]]; then
+		return 1
+	fi
+
+	# Need to rebuild
+	return 0
+}
 
 for SHADER in ${SHADER_SRCS}; do
 	SHADER_NAME=$(basename ${SHADER})
 	OUT_SHADER="${OUTPUT_SHADER_DIR}/${SHADER_NAME}.spv"
 
-	echo "Compiling shader ${SHADER}"
-	glslc "${SHADER}" -o "${OUT_SHADER}"
+	if needs_rebuild "${SHADER}" "${OUT_SHADER}"; then
+		echo "Compiling ${SHADER} ..."
+		glslc "${SHADER}" -o "${OUT_SHADER}"
+	else
+		echo "${SHADER} up to date"
+	fi
 done
 
-C_SOURCES=$(find "${SRC_DIR}" -name "*.c")
-LIB_SOURCES=$(find "${LIBS_DIR}" -name "*.c")
+for SRC in ${C_SOURCES} ${LIB_SOURCES}; do
+	OBJ_FILE="${OBJ_DIR}/$(basename ${SRC} .c).o"
+	OBJ_FILES+=("${OBJ_FILE}")
 
-CFLAGS=" -g -Wall -Wextra -Wshadow -Wpedantic -DDEBUG=1 -DOS_LINUX=1 -std=gnu17"
-LDFLAGS="-lglfw -lvulkan -lm"
+	if needs_rebuild "${SRC}" "${OBJ_FILE}"; then
+		echo "Compiling ${SRC} ..."
+		gcc ${CFLAGS} -I${SRC_DIR} -I${LIBS_DIR} -c "${SRC}" -o "${OBJ_FILE}"
+	else
+		echo "${SRC} up to date"
+	fi
+done
 
-echo "Compiling C sources..."
-gcc ${CFLAGS} -I${SRC_DIR} -I${LIBS_DIR} ${C_SOURCES} ${LDFLAGS} -o "${BIN_DIR}/${PROJECT_NAME}"
+if [[ ${#OBJ_FILES[@]} -gt 0 ]]; then
+	echo "Linking..."
+	gcc ${CFLAGS} "${OBJ_FILES[@]}" ${LDFLAGS} -o "${BIN_DIR}/${PROJECT_NAME}"
+else
+	echo "Project up to date"
+fi
 
-echo "Build Complete... ${BIN_DIR}/${PROJECT_NAME}"
+echo "Build Complete... (${BIN_DIR}/${PROJECT_NAME})"
